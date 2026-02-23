f9842b9d544b16b6020b00147b8a291e2a1fdca4
Atul Yadav <150727962+AtulYadavCodes@users.noreply.github.com>
2026-02-24 02:14:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ import express from 'express';
 const app=express();
 const PORT=3000;
 app.get('/',(req,res)=>{res.send("hello")});
+app.get('/vpsservices',(req,res)=>{res.send("vpsse...on")});
 app.listen(PORT,()=>{console.log("strted")});
